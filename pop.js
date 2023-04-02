9cefdac2696116af7ad81059495eea691426bfd1
Sebastian Ramos <sebas.ramos.villanueva@gmail.com>
2023-04-02 17:06:06 -0500

    subiendo api

--- pop.js
+++ pop.js
@@ -1,33 +1,20 @@
 var btnAbrirPopup = document.getElementById('open'),
-	overlay = document.getElementById('overlay'),
-	popup = document.getElementById('popup'),
-	btnCerrarPopup = document.getElementById('btn-cerrar-popup'),
+  overlay = document.getElementById('overlay'),
+  popup = document.getElementById('popup'),
+  btnCerrarPopup = document.getElementById('btn-cerrar-popup'),
+  btnAbrirChat = document.querySelector('.btn-open-chat');
 
-	btnAbrirChat = document.querySelector('.btn-open-chat'),
-	overlayChat = document.querySelector('.overlay-chat'),
-	overlayChatContent = document.querySelector('.overlay-chat-content'),
-	btnCerrarChat = document.querySelector('.btn-cerrar-chat');
-
-btnAbrirPopup.addEventListener('click', function(){
-	overlay.classList.add('active');
-	popup.classList.add('active');
+btnAbrirPopup.addEventListener('click', function () {
+  overlay.classList.add('active');
+  popup.classList.add('active');
 });
 
-btnCerrarPopup.addEventListener('click', function(e){
-	e.preventDefault();
-	overlay.classList.remove('active');
-	popup.classList.remove('active');
+btnCerrarPopup.addEventListener('click', function (e) {
+  e.preventDefault();
+  overlay.classList.remove('active');
+  popup.classList.remove('active');
 });
 
-btnAbrirChat.addEventListener('click', function(){
-	console.log("Abriendo chat");
-	overlayChat.classList.add('active');
-	overlayChatContent.classList.add('active');
-});
+const abrir = () => {};
 
-btnCerrarChat.addEventListener('click', function(e){
-	e.preventDefault();
-	overlayChat.classList.remove('active');
-	overlayChatContent.classList.remove('active');
-});
-
+btnAbrirChat.addEventListener('click', abrir);
